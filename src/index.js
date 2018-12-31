f1cb789752fe063bc46c804573b92fcb5be16c99
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2018-12-31 14:09:19 +1100

    Created a dir for components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Root from './Root';
+import Root from './components/Root';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
