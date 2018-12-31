eb0b9f74f3ee2e91e95f1e9f32a6bed83d9f9e8c
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2018-12-31 19:01:24 +1100

    Added pre-loading of stats and equipment types & stats

--- src/reducers/entities/ids.js
+++ src/reducers/entities/ids.js
@@ -1,7 +1,12 @@
 import {
 	GET_ARTIFACTS_SUCCESS,
+	GET_EQUIPMENT_STATS,
+	GET_EQUIPMENT_TYPES,
+	GET_EQUIPMENT_TYPES_SUCCESS,
 	GET_HEROES_SUCCESS,
 	GET_RESOURCES_SUCCESS,
+	GET_STATS,
+	GET_STATS_SUCCESS,
 	GET_STATUS_EFFECTS_SUCCESS,
 } from '../../actionTypes';
 
@@ -26,6 +31,16 @@ export default (state = {}, action) => {
 			return {
 				...state,
 				resources: [...action.payload.result],
+			};
+		case GET_STATS_SUCCESS:
+			return {
+				...state,
+				stats: [...action.payload.result],
+			};
+		case GET_EQUIPMENT_TYPES_SUCCESS:
+			return {
+				...state,
+				equipmentTypes: [...action.payload.result],
 			};
 		default:
 			return state;
