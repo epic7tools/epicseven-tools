0138ab2eeb5808ace859f6516c8fda952495d5bb
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2018-12-31 15:21:03 +1100

    Normalized hero data stored in state

--- src/actions/initialise.js
+++ src/actions/initialise.js
@@ -1,6 +1,5 @@
 import getHeroes from './requests/getHeroes';
 
 export default () => dispatch => {
-	console.log('in init');
 	dispatch(getHeroes());
 };
--- src/reducers/entities.js
+++ src/reducers/entities.js
@@ -1,16 +1,20 @@
 import {GET_HEROES_SUCCESS} from '../actionTypes';
+import merge from 'lodash.merge';
 
 const initialState = {
+	byId: {},
 	heroes: [],
 };
 
 export default (state = initialState, action) => {
 	switch (action.type) {
-		case GET_HEROES_SUCCESS:
+		case GET_HEROES_SUCCESS: {
 			return {
 				...state,
-				heroes: [...action.payload],
+				byId: merge({}, action.payload.entities, state.byId),
+				heroes: [...action.payload.result.filter(id => id !== '_HERO_TEMPLATE')],
 			};
+		}
 		default:
 			return state;
 	}
