23488e841db41d36c4c53758243b627c168c80d2
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2019-01-01 22:12:01 +1100

    Added hero selectors

---
+++ src/selectors/getHeroList.js
@@ -0,0 +1,11 @@
+import {createSelector} from 'reselect';
+import getHeroes from './getHeroes';
+
+export default createSelector(
+	getHeroes,
+	heroes =>
+		heroes.map(hero => ({
+			label: hero.name,
+			value: hero.fileId,
+		}))
+);
