02d863c26aa1fbe7cb4b7c25a3d7942cde82b677
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2019-01-01 23:50:49 +1100

    Setup Main to push below the appbar

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,17 @@
 import {withStyles} from '@material-ui/core/styles';
 import React from 'react';
 
-const styles = {
-	root: {},
-};
+const styles = theme => ({
+	root: {
+		flexGrow: 1,
+	},
+	appBarSpacer: theme.mixins.toolbar,
+});
 
-const Main = ({classes}) => <main className={classes.root} />;
+const Main = ({classes}) => (
+	<main className={classes.root}>
+		<div className={classes.appBarSpacer} />
+	</main>
+);
 
 export default withStyles(styles)(Main);
