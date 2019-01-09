4a5f33c698bfd7866e3510196c7f1dcc40315546
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2019-01-09 21:07:10 +1100

    wip

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,7 @@ import {withStyles} from '@material-ui/core/styles';
 import * as PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import HeroSelect from '../HeroSelect';
-import HeroStars from '../HeroStars/HeroStars';
+import HeroView from '../HeroView';
 
 const styles = theme => ({
 	root: {
@@ -26,7 +26,7 @@ class Main extends Component {
 			<main className={classes.root}>
 				<div className={classes.appBarSpacer} />
 				<HeroSelect value={this.state.value} onChange={this.handleChange} />
-				<HeroStars awakened={5} total={6} />
+				<HeroView />
 			</main>
 		);
 	}
