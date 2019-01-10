d7004b568dcbee4bdfaf948a5f6e0a74aed9a877
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2019-01-10 21:45:10 +1100

    Added dark theme

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,14 @@
+import MuiThemeProvider from '@material-ui/core/es/styles/MuiThemeProvider';
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from '../store';
+import theme from '../util/theme';
 import App from './App';
 
 export default () => (
 	<Provider store={store}>
-		<App />
+		<MuiThemeProvider theme={theme}>
+			<App />
+		</MuiThemeProvider>
 	</Provider>
 );
---
+++ src/util/theme.js
@@ -0,0 +1,7 @@
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
+export default createMuiTheme({
+	palette: {
+		type: 'dark',
+	},
+});
