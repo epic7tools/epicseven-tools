3fc6c79867b96a39e81f51f79989e4b4fb082368
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2019-01-16 20:54:24 +1100

    Made zodiac into a button with icons

---
+++ src/core/components/ZodiacButton/ZodiacButton.js
@@ -0,0 +1,83 @@
+import * as PropTypes from 'prop-types';
+import React from 'react';
+import ImageButtonBase from '../ImageButtonBase';
+import aquarius from './assets/aquarius.png';
+import aries from './assets/aries.png';
+import cancer from './assets/cancer.png';
+import gemini from './assets/gemini.png';
+import leo from './assets/leo.png';
+import libra from './assets/libra.png';
+import pisces from './assets/pisces.png';
+import taurus from './assets/taurus.png';
+import virgo from './assets/virgo.png';
+
+const variants = {
+	capricorn: {
+		label: 'Capricorn',
+	},
+	virgo: {
+		label: 'Virgo',
+		icon: virgo,
+	},
+	libra: {
+		label: 'Libra',
+		icon: libra,
+	},
+	gemini: {
+		label: 'Gemini',
+		icon: gemini,
+	},
+	pisces: {
+		label: 'Pisces',
+		icon: pisces,
+	},
+	leo: {
+		label: 'Leo',
+		icon: leo,
+	},
+	cancer: {
+		label: 'Cancer',
+		icon: cancer,
+	},
+	sagittarius: {
+		label: 'Sagittarius',
+	},
+	aquarius: {
+		label: 'Aquarius',
+		icon: aquarius,
+	},
+	aries: {
+		label: 'Aries',
+		icon: aquarius,
+	},
+	taurus: {
+		label: 'Taurus',
+		icon: taurus,
+	},
+	scorpio: {
+		label: 'Scorpio',
+	},
+};
+
+const ZodiacButton = ({variant, ...props}) => (
+	<ImageButtonBase src={variants[variant].icon} label={variants[variant].label} {...props} />
+);
+
+ZodiacButton.propTypes = {
+	variant: PropTypes.oneOf([
+		'capricorn',
+		'virgo',
+		'libra',
+		'gemini',
+		'pisces',
+		'leo',
+		'cancer',
+		'sagittarius',
+		'aquarius',
+		'aries',
+		'taurus',
+		'scorpio',
+	]),
+};
+
+export default ZodiacButton;
--- src/gear-preview/components/Zodiac/index.js
+++
@@ -1,3 +0,0 @@
-import Zodiac from './Zodiac';
-
-export default Zodiac;
