8c31a0389c84260649d9e6e22253c483625778cc
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2019-02-21 15:46:10 +1100

    Fixed bug for updated percentages in stats

--- src/gear-preview/components/HeroStats/formatValueText.js
+++ src/gear-preview/components/HeroStats/formatValueText.js
@@ -1,3 +1,4 @@
 // Math.abs used to ensure a positive value so no sign is printed
 // so we can manually add the sign if we want to
-export default (value, percentage) => `${Math.abs(Math.floor(value))}${percentage ? '%' : ''}`;
+export default (value, percentage) =>
+	`${Math.abs(Math.round(percentage ? value * 100 : value))}${percentage ? '%' : ''}`;
--- src/gear-preview/selectors/getSelectedHeroAwakenedStats.js
+++ src/gear-preview/selectors/getSelectedHeroAwakenedStats.js
@@ -3,13 +3,7 @@ import {statsById} from '../../core/constants/stats';
 import getSelectedHeroAwakenings from './getSelectedHeroAwakenings';
 import getSelectedHeroBaseStats from './getSelectedHeroBaseStats';
 
-// handles deprecated "3%" style strings
-const getIncrease = value => {
-	if (typeof value !== 'string') {
-		return value;
-	}
-	return value.endsWith('%') ? parseFloat(value) / 100 : parseInt(value);
-};
+const isInt = num => num % 1 === 0;
 
 export default createSelector(
 	getSelectedHeroBaseStats,
@@ -20,18 +14,14 @@ export default createSelector(
 		};
 		awakenings.forEach(awakening => {
 			awakening.statsIncrease.forEach(increase => {
-				Object.entries(increase).forEach(i => {
-					const stat = i[0];
-					const value = getIncrease(i[1]);
+				Object.entries(increase).forEach(([stat, value]) => {
 					// handling some bad data with empty keys
 					if (stat) {
-						if (value < 1) {
-							newBaseStats[stat] += statsById[stat].percentage
-								? value * 100
-								: baseStats[stat] * value;
-						} else {
-							newBaseStats[stat] += value;
-						}
+						// if we are not working with a percentage stat
+						// AND the value is not an integer, assume that we are adding
+						// a percentage of the base stats
+						newBaseStats[stat] +=
+							!statsById[stat].percentage && !isInt(value) ? baseStats[stat] * value : value;
 					}
 				});
 			});
