893fb7ab78758e0af6e1e668b1c54a903ff4b8ed
stocky37 <2060486+stocky37@users.noreply.github.com>
2019-02-22 12:10:06 +1100

    Added artifact stat selection (#80)

---
+++ src/gear-preview/components/ArtifactInput/ArtifactInput.js
@@ -0,0 +1,36 @@
+import Paper from '@material-ui/core/Paper';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Typography from '@material-ui/core/Typography';
+import classNames from 'classnames';
+import React from 'react';
+import {atk, hp} from '../../../core/constants/stats';
+import GearStatInput from '../GearStatInput';
+
+const style = theme => ({
+	root: {
+		padding: theme.spacing.unit * 2,
+		minWidth: 340,
+	},
+});
+
+const ArtifactInput = ({className, classes, name, onChange, values, ...props}) => (
+	<Paper className={classNames(classes.root, className)} {...props}>
+		<Typography variant="h5">Artifact</Typography>
+		<GearStatInput
+			name="stat1"
+			label="Stat #1"
+			stats={[atk]}
+			onChange={onChange}
+			values={values.stat1}
+		/>
+		<GearStatInput
+			name="stat2"
+			label="Stat #2"
+			stats={[hp]}
+			onChange={onChange}
+			values={values.stat2}
+		/>
+	</Paper>
+);
+
+export default withStyles(style)(ArtifactInput);
