637da2d6992af99fd5cd6da0c6930f7fad2dd2da
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2019-03-26 15:58:17 +1100

    Removed redux-batched-actions

--- src/core/store/prod/middleware.js
+++ src/core/store/prod/middleware.js
@@ -1,5 +1,4 @@
 import {apiMiddleware} from 'redux-api-middleware';
 import thunk from 'redux-thunk';
-import {batchDispatchMiddleware} from 'redux-batched-actions';
 
-export default [batchDispatchMiddleware, apiMiddleware, thunk];
+export default [apiMiddleware, thunk];
