481ab1ec44076025e09bd9cb74796786b76128af
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-10 10:38:40 +0000

    Deploy epic7tools/epicseven-tools to github.com/epic7tools/epicseven-tools.git:gh-pages

---
+++ precache-manifest.97cdb0a271c805dbb8b1c34e962841bf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5365a12456bda89248c123a0d12be732",
+    "url": "https://epic7tools.github.io/epicseven-tools/index.html"
+  },
+  {
+    "revision": "fadfea5987a0e7588525",
+    "url": "https://epic7tools.github.io/epicseven-tools/static/js/2.b6b3bacf.chunk.js"
+  },
+  {
+    "revision": "4e6757329c15af085935",
+    "url": "https://epic7tools.github.io/epicseven-tools/static/js/main.4eadce83.chunk.js"
+  },
+  {
+    "revision": "5a5b349132f822fa8a51",
+    "url": "https://epic7tools.github.io/epicseven-tools/static/js/runtime~main.24d80abc.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://epic7tools.github.io/epicseven-tools/precache-manifest.97cdb0a271c805dbb8b1c34e962841bf.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://epic7tools.github.io/epicseven-tools/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
